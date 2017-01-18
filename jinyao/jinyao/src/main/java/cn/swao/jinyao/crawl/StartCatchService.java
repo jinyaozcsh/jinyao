ac9b8772026d065195f05e1fbad9c54f7ee2a2c7
SJ <SJ>
2017-01-18 17:44:11 +0800

    社区活动抓爬

--- jinyao/jinyao/src/main/java/cn/swao/jinyao/crawl/StartCatchService.java
+++ jinyao/jinyao/src/main/java/cn/swao/jinyao/crawl/StartCatchService.java
@@ -1,13 +1,17 @@
 package cn.swao.jinyao.crawl;
 
-import java.util.*;
+import java.util.Hashtable;
+import java.util.Map;
 
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
-import cn.swao.jinyao.crawl.special.*;
+import cn.swao.jinyao.crawl.special.ActualNewsProcessor;
 import cn.swao.jinyao.crawl.special.ActualNewsProcessor.IActualNewsSave;
+import cn.swao.jinyao.crawl.special.CommunityActivityProcessor;
+import cn.swao.jinyao.crawl.special.CommunityActivityProcessor.ICommunityActivitySave;
+import cn.swao.jinyao.crawl.special.SoundNewsProcessor;
 import cn.swao.jinyao.crawl.special.SoundNewsProcessor.ISoundNewsSave;
 import us.codecraft.webmagic.Spider;
 
@@ -18,18 +22,22 @@ import us.codecraft.webmagic.Spider;
  * @desc desc:爬虫启动类
  */
 @Service
-public class StartCatchService implements IActualNewsSave, ISoundNewsSave {
+public class StartCatchService implements IActualNewsSave, ISoundNewsSave ,ICommunityActivitySave{
 
     @Autowired
     private ActualNewsProcessor actualNewsProcessor;
 
     @Autowired
     private SoundNewsProcessor soundNewsProcessor;
+    
+    @Autowired
+    private CommunityActivityProcessor communityActivityProcessor; 
 
     @Test
     public void test() {
         // startActualNews();
-        startSoundNews();
+//        startSoundNews();
+        startCommunityActivity();
     }
 
     /**
@@ -52,6 +60,19 @@ public class StartCatchService implements IActualNewsSave, ISoundNewsSave {
         SoundNewsProcessor soundNewsProcessor = new SoundNewsProcessor();
         soundNewsProcessor.setSaveCallBack(this);
         Spider.create(soundNewsProcessor).addUrl(SoundNewsProcessor.START_URL).run();
+        // Spider spider = Spider.create(soundNewsProcessor);
+        // spider.addUrl(SoundNewsProcessor.START_URL);
+        // spider.start();
+
+    }
+    
+    /**
+     * 启动语音新闻抓爬
+     */
+    public void startCommunityActivity() {
+        CommunityActivityProcessor communityActivityProcessor = new CommunityActivityProcessor();
+        communityActivityProcessor.setSaveCallBack(this);
+        Spider.create(communityActivityProcessor).addUrl(CommunityActivityProcessor.START_URL).run();
         // Spider spider = Spider.create(soundNewsProcessor);
         // spider.addUrl(SoundNewsProcessor.START_URL);
         // spider.start();
@@ -87,6 +108,14 @@ public class StartCatchService implements IActualNewsSave, ISoundNewsSave {
      */
     @Override
     public void soundNewsSave(Hashtable<String, Object> table) {
+        actualNewsSave(table);
+    }
+
+    /**
+     * 社区活动新闻保存回调
+     */
+    @Override
+    public void communityActivitysvae(Hashtable<String, Object> table) {
         actualNewsSave(table);
     }
 }
